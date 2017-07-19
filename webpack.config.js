000788ff5b8f0bfd5082d604c412f6b82e3e1dfa
minajevs <d3x133@gmail.com>
2017-07-19 11:01:37 +0300

    Updated packages

--- webpack.config.js
+++ webpack.config.js
@@ -12,17 +12,21 @@ module.exports = {
     plugins: [new HtmlWebpackPlugin()],
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
     module: {
-        loaders: [
+        rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
-        ],
-
-        preLoaders: [
+            { 
+                test: /\.tsx?$/, 
+                loader: "awesome-typescript-loader" 
+            },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                loader: "source-map-loader"
+            }
         ]
     },
 };
