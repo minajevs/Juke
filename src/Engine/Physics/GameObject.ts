58036b9f199ec4aee934978b77d62be75d4733b1
minajevs <d3x133@gmail.com>
2017-07-19 12:24:37 +0300

    Documented gameobjec and rect

--- src/Engine/Physics/GameObject.ts
+++ src/Engine/Physics/GameObject.ts
@@ -5,17 +5,51 @@ import Tools from "../Tools/Tools";
 import Core from "../Core/Core";
 import Tick from "../Core/Tick";
 
-export enum EnumLayer {
+/**
+ * Enum for game layers. Game works with layers 1 by 1 from low to high.
+ * @export
+ * @enum {number}
+ */
+export enum Layer { //TODO: Move to core?
+    /**
+     * Default / Unassigned
+     */
     default = 0,
+    /**
+     * Used for object on background (skies, space, universe etc.)
+     */
     background = 1,
+    /**
+     * Used for object on ground/main layer
+     */
     ground = 2,
+    /**
+     * Used for object on the foreground (tree leaves, roofs etc.)
+     */
     foreground = 3,
+    /**
+     * Used for object which should be rendered last (shaders ???)
+     */
     special = 4
 }
 
+/**
+ * Enum for built-in GameObject types. 
+ * @export
+ * @enum {number}
+ */
 export enum Roles {
+    /**
+     * Default / Unassigned
+     */
     default = 0,
+    /**
+     * Not visible, participates in 2d physics, collisions
+     */
     collider = 1,
+    /**
+     * Visible, does not participate in 2d physics
+     */
     sprite = 2,
 }
 
@@ -24,23 +58,67 @@ export interface IGameObjectOptions extends IRectOptions {
     role?: Roles;
     renderable?: boolean;
     sprite?: Sprite;
-    layer?: EnumLayer;
+    layer?: Layer;
     children?: Array<GameObject>;
     parent?: GameObject;
     collide?: boolean;
 }
 
+/**
+ * Base game engine object class.  
+ * @export
+ * @class GameObject
+ * @extends {Rect}
+ */
 export default class GameObject extends Rect {
+    /**
+     * Free text. Can be used to identify object from others 
+     * @type {string}
+     * @memberof GameObject
+     */
     tag: string = undefined;
-    role: Roles = Roles.default;
-    renderable: boolean = false;
-    sprite: Sprite; //TODO: add some default sprite
-    layer: EnumLayer = 0;
-    get colliders(): GameObject[] {
-        return this._children[Roles.collider];
-    };
 
+    /**
+     * Object type
+     * @type {Roles}
+     * @memberof GameObject
+     */
+    role: Roles = Roles.default;
+
+    /**
+     * Tells engine if this object should be rendered
+     * @type {boolean}
+     * @memberof GameObject
+     */
+    renderable: boolean = false;
+
+    /**
+     * Game object sprite
+     * @type {Sprite}
+     * @memberof GameObject
+     */
+    sprite: Sprite; //TODO: add some default sprite
+
+    /**
+     * Tells on which layer object is located. The higher leyer is - the later object will be taken care of
+     * @type {Layer}
+     * @memberof GameObject
+     */
+    layer: Layer = 0;
+
+    /**
+     * Private children dictionary. Children are stored in dictionary by roles. Keys: Role / Values: Array of objects
+     * @private
+     * @type {{ [role: number]: Array<GameObject> }}
+     * @memberof GameObject
+     */
     private _children: { [role: number]: Array<GameObject> } = {};
+    
+    /**
+     * All game object children
+     * @type {Array<GameObject>}
+     * @memberof GameObject
+     */
     get children(): Array<GameObject> {
         let ret:Array<GameObject> = [];
         for (let role in Roles){
@@ -53,7 +131,30 @@ export default class GameObject extends Rect {
             this.addChild(obj);
     }
 
+
+    /**
+     * Game object children of "Collider" type
+     * @readonly
+     * @type {GameObject[]}
+     * @memberof GameObject
+     */
+    get colliders(): GameObject[] {
+        return this._children[Roles.collider];
+    };
+
+    /**
+     * Internal game object parent reference
+     * @private
+     * @type {GameObject}
+     * @memberof GameObject
+     */
     private _parent: GameObject;
+
+    /**
+     * Parent object
+     * @type {GameObject}
+     * @memberof GameObject
+     */
     get parent(): GameObject { return this._parent; }
     set parent(value: GameObject) {
         let parent = this._parent;
@@ -66,17 +167,46 @@ export default class GameObject extends Rect {
         }
     }
 
+    /**
+     * Object which game object is "locked" to (position locked)
+     * @type {GameObject}
+     * @memberof GameObject
+     */
     positionLock: GameObject;
 
+    /**
+     * Internal near object reference. Is tuple of near objects and reference time frame (tick)
+     * @private
+     * @type {[Array<GameObject>, number]}
+     * @memberof GameObject
+     */
     private _nearObjects: [Array<GameObject>, number] = [[], 0]; //Objectlist , last update tick
+
+    /**
+     * Object located nearby
+     * @readonly
+     * @type {Array<GameObject>}
+     * @memberof GameObject
+     */
     get nearObjects(): Array<GameObject> {
         if (this._nearObjects[1] != this.tick.tick)
             this._nearObjects = [this.tick.map.getNearby(this), this.tick.tick];
         return this._nearObjects[0];
     }
 
-    tick: Tick;
+    /**
+     * Local backup of the current tick
+     * @private
+     * @type {Tick}
+     * @memberof GameObject
+     */
+    private tick: Tick;
 
+    /**
+     * Creates an instance of GameObject.
+     * @param {IGameObjectOptions} [options] Optional GameObject creation options
+     * @memberof GameObject
+     */
     constructor(options?: IGameObjectOptions) {
         super(options);
         Tools.extend(this, options);
@@ -99,6 +229,11 @@ export default class GameObject extends Rect {
             if(this._children[role] == null) this._children[role] = []
     }
 
+    /**
+     * Updates GameObject state for provided tick
+     * @param {Tick} tick Game tick
+     * @memberof GameObject
+     */
     update(tick: Tick) {
         this.tick = tick;
         if (this.positionLock != null) this.pos = this.positionLock.pos;
@@ -108,7 +243,12 @@ export default class GameObject extends Rect {
 
     }
 
-    addChild(object: GameObject) {
+    /**
+     * Adds child to object
+     * @param {GameObject} object
+     * @memberof GameObject
+     */
+    addChild(object: GameObject):void {
         if (this._children[object.role] == null) this._children[object.role] = new Array<GameObject>();
         if (this._children[object.role].indexOf(object) > -1) return;
 
@@ -117,6 +257,11 @@ export default class GameObject extends Rect {
         object._parent = this;
     }
 
+    /**
+     * Removes child from object
+     * @param {GameObject} object 
+     * @memberof GameObject
+     */
     removeChild(object: GameObject) {
         if (this._children[object.role] == null || this._children[object.role].indexOf(object) == -1) return;
         let index = this._children[object.role].indexOf(object);
@@ -125,10 +270,21 @@ export default class GameObject extends Rect {
         this._children[object.role].splice(index, 1);
     }
 
+    /**
+     * Returns object rect (position, dimensions)
+     * @returns {Rect} 
+     * @memberof GameObject
+     */
     getRect(): Rect {
         return new Rect({ pos: this.pos, w: this.w, h: this.h });
     }
 
+    /**
+     * Tells if object collides with any or provided object
+     * @param {GameObject} [object] Check collision only with provided object
+     * @returns {boolean} 
+     * @memberof GameObject
+     */
     collides(object?: GameObject): boolean {
         let colliders = this.colliders;
         if (colliders.length < 1 || this.tick == null) return false;
@@ -148,6 +304,11 @@ export default class GameObject extends Rect {
         }
     }
 
+    /**
+     * Get all objects with which object collides
+     * @returns {Array<GameObject>} 
+     * @memberof GameObject
+     */
     collisions(): Array<GameObject> {
         let ret = new Array<GameObject>();
         for (let obj of this.nearObjects) {
@@ -160,11 +321,21 @@ export default class GameObject extends Rect {
         return ret;
     }
 
+    /**
+     * Adds to current position
+     * @param {Vector} pos 
+     * @memberof GameObject
+     */
     moveBy(pos: Vector) {
         this.pos._plus(pos);
         //if(this.collider) this.collider.pos._plus(pos);
     }
 
+    /**
+     * Sets current vector
+     * @param {Vector} pos 
+     * @memberof GameObject
+     */
     moveTo(pos: Vector) {
         let delta = Vector._minus(pos, this.pos);
         this.moveBy(delta);
--- src/Engine/Physics/Rect.ts
+++ src/Engine/Physics/Rect.ts
@@ -8,21 +8,58 @@ export interface IRectOptions{
     pos2?: Vector;
 }
 
+/**
+ * Base geometric class. Represents 2d rectangular with vector position and 2 dimensions (width, height)
+ * @export
+ * @class Rect
+ */
 export default class Rect{
-    //Physical parameters
+    /**
+     * Width
+     * @type {number}
+     * @memberof Rect
+     */
     w: number = 0;
+
+    /**
+     * Height
+     * @type {number}
+     * @memberof Rect
+     */
     h: number = 0;
-    //Top-left coordinates
+
+    /**
+     * Top-left corner position of rectangular
+     * @type {Vector}
+     * @memberof Rect
+     */
     pos: Vector = new Vector(0, 0);
     //Bottom-right coordinates
+
+    /**
+     * Right-bottom position of rectangular
+     * @type {Vector}
+     * @memberof Rect
+     */
     get pos2(): Vector {return Vector._plus(this.pos, new Vector(this.w, this.h)); }
     set pos2(value: Vector){
         this.w = value.x - this.pos.x;
         this.h = value.y - this.pos.y;
-        console.log(this.w);
     }
+
+    /**
+     * Rectangular area
+     * @readonly
+     * @type {number}
+     * @memberof Rect
+     */
     get area(): number {return (this.pos2.x - this.pos.x) * (this.pos2.y - this.pos.y); }
-    get largeArea(): number {return (this.pos2.x - this.pos.x) * (this.pos2.y - this.pos.y); }
+
+    /**
+     * Center of rectangular
+     * @type {Vector}
+     * @memberof Rect
+     */
     get center(): Vector {return Vector._plus(this.pos, new Vector(this.w / 2, this.h / 2)); }
     set center(value: Vector) {this.pos.x = value.x - (this.w / 2); this.pos.y = value.y - (this.h / 2); }
 
@@ -30,6 +67,12 @@ export default class Rect{
         Tools.extend(this, options);
     }
 
+    /**
+     * Tells if rectangular intersects with provided rectangular
+     * @param {Rect} rect 
+     * @returns {boolean} 
+     * @memberof Rect
+     */
     intersects(rect: Rect): boolean{
         return !(this.pos.x > rect.pos2.x ||
                 this.pos2.x < rect.pos.x ||
